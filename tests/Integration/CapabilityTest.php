9fbeb02390f0f1d9cd59c3dc29acca32639752c7
Gary Jones <github@garyjones.io>
2023-06-27 14:50:06 +0100

    Integration Tests: Use static factory method
    WordPress 4.4 and above has had the old `$factory` property changed to a `factory()` method.

--- tests/Integration/CapabilityTest.php
+++ tests/Integration/CapabilityTest.php
@@ -109,7 +109,7 @@ final class CapabilityTest extends TestCase {
 	 * @return bool True if the role has the redirects capability, false otherwise.
 	 */
 	private function assertRoleHasRedirectsCapability( $role ) {
-		$user_id = $this->factory->user->create( array( 'role' => $role ) );
+		$user_id = self::factory()->user->create( array( 'role' => $role ) );
 		$user    = wp_set_current_user( $user_id );
 
 		return $this->assertUserHasRedirectCapability( $user );
@@ -122,7 +122,7 @@ final class CapabilityTest extends TestCase {
 	 * @return bool True if the role does not have the redirects capability, false otherwise.
 	 */
 	private function assertRoleNotHasRedirectsCapability( $role ) {
-		$user_id = $this->factory->user->create( array( 'role' => $role ) );
+		$user_id = self::factory()->user->create( array( 'role' => $role ) );
 		$user    = wp_set_current_user( $user_id );
 
 		return $this->assertUserNotHasRedirectCapability( $user );
